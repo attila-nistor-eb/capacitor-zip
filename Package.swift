3d905146fcb63c6bfbdfaf1ed7f2b87df662ec0d
Attila Nistor <attila.nistor@everbridge.com>
2026-09-11 11:53:38 +0200

    Use custom ZIPFoundation fork for iOS

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.19")
+        .package(url: "https://github.com/attila-nistor-eb/ZIPFoundation.git", branch: "codex/memory-safe-unzip")
     ],
     targets: [
         .target(
